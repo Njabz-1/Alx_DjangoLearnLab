3cee0749dc78dcafc3ec26fca96344c71cfbfad5
Njabulo <mhlambinjabulo@gmail.com>
2024-08-18 19:09:20 +0200

    Add UserCreation explicitly

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -31,6 +31,10 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
 
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm() 
+        return render(request, self.template_name, {'form': form})
+
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)  
