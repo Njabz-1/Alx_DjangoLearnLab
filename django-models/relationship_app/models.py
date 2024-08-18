6e0efb3ceb8ec1328e9568181461811aced0b599
Njabulo <mhlambinjabulo@gmail.com>
2024-08-18 19:55:32 +0200

    Task 4

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -8,6 +8,13 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
