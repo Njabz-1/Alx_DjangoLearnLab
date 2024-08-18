bbe5d0d9bbd102071d571ee17bf472731a91dc32
Njabulo <mhlambinjabulo@gmail.com>
2024-08-18 17:00:38 +0200

    Add view import

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, CustomLoginView, CustomLogoutView, RegisterView
+from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', views.CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
