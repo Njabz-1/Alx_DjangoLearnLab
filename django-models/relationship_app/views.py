c8f80ed69ff61c14060c83b24b0e15e7d647036d
Njabulo <mhlambinjabulo@gmail.com>
2024-08-18 16:56:25 +0200

    Add login import

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -29,3 +30,8 @@ class RegisterView(CreateView):
     template_name = 'relationship_app/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)  
+        return super().form_valid(form)
