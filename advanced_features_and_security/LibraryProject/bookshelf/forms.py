e2c65b6e58e8c5ede13b50c1e31cc7a18c970cfd
Njabulo <mhlambinjabulo@gmail.com>
2024-08-25 20:08:45 +0200

    Third task

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']
+class ExampleForm(forms.Form):
+    nama = forms.CharField(label='name', max_length=100)
+    email = forms.EmailField(label="email")
+    massage = forms.CharField(widget=forms.Textarea, label="Your massage")
+
+
+class BookSearchForm(forms.Form):
+    query = forms.CharField(max_length=100, required=False)
