645a304f8a9d4043a8780b10f97ead2a66caeb3c
Njabulo <mhlambinjabulo@gmail.com>
2024-08-26 21:48:17 +0200

    Added include import

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
@@ -8,4 +8,5 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
