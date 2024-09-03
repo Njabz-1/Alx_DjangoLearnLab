c8e8ee20d1c79dad9d91c0f6a8524b80607ebbfb
Njabulo <mhlambinjabulo@gmail.com>
2024-09-03 16:33:59 +0200

    Added Bookfilter class

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,14 +4,28 @@ from .models import Book
 from .seriealizers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters import rest_framework
+from django_filters import rest_framework as filters 
+class BookFilter(filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            'title': ['exact', 'icontains'],
+            'author__name': ['exact', 'icontains'],
+            'publication_year': ['exact', 'gte', 'lte'],
+        }
+    ordering = filters.OrderingFilter(
+        fields=(
+            ('title', 'title'),
+            ('publication_year', 'publication_year'),
+        )
+    )
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['title', 'author__name', 'publication_year'] 
+    filterset_class = BookFilter
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
 
