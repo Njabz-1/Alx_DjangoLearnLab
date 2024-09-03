2abd46a528ab314b4b4420c734e68ab9447dda32
Njabulo <mhlambinjabulo@gmail.com>
2024-09-03 15:42:30 +0200

    Add task 3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .seriealizers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -9,9 +10,19 @@ class BookListView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles listing and creating books with additional functionalities:
+    - Filtering by title, author, and publication year.
+    - Searching by title and author name.
+    - Ordering by title and publication year.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author__name', 'publication_year'] 
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
