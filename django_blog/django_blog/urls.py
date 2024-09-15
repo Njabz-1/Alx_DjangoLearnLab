a95efae8bef49aaaf5c8bdb3550bfc9b19062741
Njabulo <mhlambinjabulo@gmail.com>
2024-09-15 10:58:46 +0200

    Second task

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -5,9 +5,5 @@ from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog_views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('register/', blog_views.register, name='register'),
-    path('profile/', blog_views.profile, name='profile'),
+    path('', include('blog.urls')),
 ]
