66a6d46774779059171d5f52fe4bc23d5c1a332f
Njabulo <mhlambinjabulo@gmail.com>
2024-09-22 11:05:24 +0200

    Inital commit of second task

---
+++ social_media_api/posts/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Post, Comment
+
+class PostsAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_post(self):
+        data = {'title': 'Test Post', 'content': 'This is a test post'}
+        response = self.client.post('/api/posts/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Post.objects.get().title, 'Test Post')
+
+    def test_create_comment(self):
+        post = Post.objects.create(author=self.user, title='Test Post', content='This is a test post')
+        data = {'post': post.id, 'content': 'This is a test comment'}
+        response = self.client.post('/api/comments/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(Comment.objects.get().content, 'This is a test comment')
